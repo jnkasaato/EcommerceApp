838cd9ba723b751834fe8758bd380a3271af6c03
jnkasaato <jnkasaato@gmail.com>
2023-07-27 13:00:14 -0400

    update acclimate link

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,23 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+//require("@nomiclabs/hardhat-waffle");
+
+const  privateKeys = process.env.PRIVATE_KEYS || ""
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    localhost: {}, 
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: privateKeys.split(','),
+    },
+    /*
+    mumbai: { 
+      url: "https://polygon-mumbai.g.alchemy.com/v2/_mQdF1FgbtohnR7GrWej4SrGyX9vX7bv",
+      accounts: `8af4bdd998e54914e8181a2c0eef0ec0ee07b8905f080464c171217fd5f5ff97`, `ba2ead27a571c33e51774195db7924dc130a960b46134b1e585cae4205da7f17`,
+    }
+    */
+  },
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,21 +1,28 @@
-
-const hre = require("hardhat")
-const { items } = require("../src/items.json")
+const hre = require("hardhat");
+const { items } = require("../src/items.json");
 
 const tokens = (n) => {
-  return ethers.utils.parseUnits(n.toString(), 'ether')
-}
+  return ethers.utils.parseUnits(n.toString(), "ether");
+};
 
 async function main() {
+  let networkName = hre.network.name;
+
+  if (networkName !== "goerli" && networkName !== "localhost") {
+    throw new Error(
+      "Invalid network. Please use 'goerli' or 'localhost' for deployment."
+    );
+  }
+
   // Setup accounts
-  const [deployer] = await ethers.getSigners()
+  const [deployer] = await ethers.getSigners();
 
-  // Deploy 
-  const Dappazon = await hre.ethers.getContractFactory("Dappazon")
-  const dappazon = await Dappazon.deploy()
-  await dappazon.deployed()
+  // Deploy
+  const Dappazon = await hre.ethers.getContractFactory("Dappazon");
+  const dappazon = await Dappazon.deploy();
+  await dappazon.deployed();
 
-  console.log(`Deployed  Contract at: ${dappazon.address}\n`)
+  console.log(`Deployed Dappazon Contract at: ${dappazon.address}\n`);
 
   // Listing items
   for (let i = 0; i < items.length; i++) {
@@ -26,16 +33,18 @@ async function main() {
       items[i].image,
       tokens(items[i].price),
       items[i].rating,
-      items[i].stock,
-    )
+      items[i].stock
+    );
 
-    await transaction.wait()
+    await transaction.wait();
 
-    console.log(`Listed item ${items[i].id}: ${items[i].name}`)
+    console.log(`Listed item ${items[i].id}: ${items[i].name}`);
   }
 }
 
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+  });
