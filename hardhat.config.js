2b2bcdcaac0ce535ea68490a25b6afe49cf8c8cb
jnkasaato <jnkasaato@gmail.com>
2023-07-28 16:35:58 -0400

    deploy mumbai

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,22 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+const  privateKeys = process.env.PRIVATE_KEYS || ""
 
-/** @type import('hardhat/config').HardhatUserConfig */
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    mumbai: { 
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: privateKeys.split(','),
+    }
+  },
 };
