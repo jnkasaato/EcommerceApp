7fec85774c7f3ecb428ac48ffd43288d6ae2af98
jnkasaato <jnkasaato@gmail.com>
2023-07-28 16:15:51 -0400

    restore

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,10 @@
 import { ethers } from 'ethers'
 
-const Navigation = ({ account, setAccount, setNetwork }) => {
-  const connectHandler = async () => {
-    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-    const account = ethers.utils.getAddress(accounts[0]);
-    setAccount(account);
+const Navigation = ({ account, setAccount }) => {
+    const connectHandler = async () => {
+        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        const account = ethers.utils.getAddress(accounts[0])
+        setAccount(account);
     }
 
     return (
@@ -12,7 +12,7 @@ const Navigation = ({ account, setAccount, setNetwork }) => {
             <div className='nav__brand'>      
                 <h1>Acclimate</h1>               
             </div>
-            <div>
+            <div >
                 <ul className='nav__links'>
                     <li><a href="#Essentials">Essentials</a></li> 
                     <li><a href="#Clothing & Costumes">Clothing</a></li>
@@ -24,23 +24,24 @@ const Navigation = ({ account, setAccount, setNetwork }) => {
                 className="nav__search"
             ></input>
 
-           <div className='nav__connect'>
             {account ? (
-              <>
-                <button type="button" className="nav__connected-button">
-                  {account.slice(0, 6) + '...' + account.slice(38, 42)}
+                <button
+                    type="button"
+                    className='nav__connect'
+                >
+                    {account.slice(0, 6) + '...' + account.slice(38, 42)}
                 </button>
-                <div className='nav__dropdown'>
-                  <button onClick={() => setNetwork('localhost')}>Localhost</button>
-                  <button onClick={() => setNetwork('goerli')}>Goerli</button>
-                </div>
-              </>
             ) : (
-              <button type="button" className="nav__connect-button" onClick={connectHandler}>
-                Connect
-              </button>
+                <button
+                    type="button"
+                    className='nav__connect'
+                    onClick={connectHandler}
+                >
+                    Connect
+                </button>
             )}
-          </div>
+
+
         </nav>
     );
 }
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,65 +1,130 @@
-import { useEffect, useState } from 'react';
-
+import { useEffect, useState } from 'react'
+import { ethers } from 'ethers'
 
 // Components
-import Rating from './Rating';
+import Rating from './Rating'
 
-import close from '../assets/close.svg';
-import image from '../items.json';
+import close from '../assets/close.svg'
+import image from '../items.json'
 
-const ethers = require("ethers")
 
-const Product = ({ item, provider, account, togglePop }) => {
-  const [order, setOrder] = useState(null);
-  const [hasBought, setHasBought] = useState(false);
+const Product = ({ item, provider, account, dappazon, togglePop }) => {
+  const [order, setOrder] = useState(null)
+  const [hasBought, setHasBought] = useState(false)
 
   const fetchDetails = async () => {
-    const ethersProvider = new ethers.providers.Web3Provider(provider);
-    const signer = ethersProvider.getSigner();
-
-    // Update the contract address for Goerli deployment
-    const contractAddress = "0x8207f62B0F8706081E4e029e6a61888b622f8b50"; // Goerli contract address
-
-    const DappazonFactory = await ethers.getContractFactory("Dappazon");
-    const dappazon = new ethers.Contract(contractAddress, DappazonFactory, signer);
-
-    const events = await dappazon.queryFilter("Buy");
+    const events = await dappazon.queryFilter("Buy")
     const orders = events.filter(
       (event) => event.args.buyer === account && event.args.itemId.toString() === item.id.toString()
-    );
+    )
 
-    if (orders.length === 0) return;
+    if (orders.length === 0) return
 
-    const order = await dappazon.orders(account, orders[0].args.orderId);
-    setOrder(order);
-  };
+    const order = await dappazon.orders(account, orders[0].args.orderId)
+    setOrder(order)
+  }
 
   const buyHandler = async () => {
-    const ethersProvider = new ethers.providers.Web3Provider(provider);
-    const signer = ethersProvider.getSigner();
-
-    // Update the contract address for Goerli deployment
-    const contractAddress = "0x8207f62B0F8706081E4e029e6a61888b622f8b50"; // Goerli contract address
-
-    const DappazonFactory = await ethers.getContractFactory("Dappazon");
-    const dappazon = new ethers.Contract(contractAddress, DappazonFactory, signer);
+    const signer = await provider.getSigner()
 
     // Buy item
-    let transaction = await dappazon.connect(signer).buy(item.id, { value: item.cost });
-    await transaction.wait();
+    let transaction = await dappazon.connect(signer).buy(item.id, { value: item.cost })
+    await transaction.wait()
 
-    setHasBought(true);
-  };
+    setHasBought(true)
+  }
 
   useEffect(() => {
-    fetchDetails();
-  }, [hasBought]);
+    fetchDetails()
+  }, [hasBought])
 
   return (
     <div className="product">
-      {/* Rest of the code remains the same... */}
-    </div>
+      <div className="product__details">
+        <div className="product__image">
+          <img src={item.image} alt="Product" />
+
+        </div>
+        <div className="product__overview">
+          <h1>{item.name}</h1>
+          <h1>{item.price}</h1>
+         
+
+          <Rating value={item.rating} /> 
+          <small>Based on 99+ ratings</small>
+          <p>{item.address}</p>
+
+          <hr />
+
+          <p>{item.address}</p>
+
+          <h2>{ethers.utils.formatUnits(item.cost.toString(), 'ether')} ETH</h2>
+
+          <hr />
+
+          <h2>Description</h2>
+
+          <p>
+            {item.description}
+            Introducing a new must-have for your furry companion! 
+            Our carefully crafted product is a game-changer that will ignite 
+            pure excitement in your pet's eyes. Made from top-quality materials
+            and a dash of  love, it is an
+            irresistible addition to your pet's world.Embrace the magic of bonding 
+            with your pet and elevate their
+            happiness with this extraordinary addition to your pet-loving home!
+          </p>
+        </div>
+
+        <div className="product__order">
+          <h1>{ethers.utils.formatUnits(item.cost.toString(), 'ether')} ETH</h1>
+
+          <p>
+            FREE delivery <br />
+            <strong>
+              {new Date(Date.now() + 349600000).toLocaleDateString(undefined, { weekday: 'long', month: 'long', day: 'numeric' })}
+            </strong>
+          </p>
+
+          {item.stock > 0 ? (
+            <p>In Stock.</p>
+          ) : (
+            <p>Out of Stock.</p>
+          )}
+
+          <button className='product__buy' onClick={buyHandler}>
+            Buy Now
+          </button>
+
+          <p><small>Ships from</small> Boston</p>
+          <p><small>Sold by</small> Acclimate</p>
+          <p><small>Return eligible through  {new Date(Date.now() + 1949600000).toLocaleDateString(undefined, { weekday: 'long', month: 'long', day: 'numeric' })}
+            </small></p>
+
+          {order && (
+            <div className='product__bought'>
+              Item bought on <br />
+              <strong>
+                {new Date(Number(order.time.toString() + '000')).toLocaleDateString(
+                  undefined,
+                  {
+                    weekday: 'long',
+                    hour: 'numeric',
+                    minute: 'numeric',
+                    second: 'numeric'
+                  })}
+              </strong>
+            </div>
+          )}
+        </div>
+
+
+        <button onClick={togglePop} className="product__close">
+          <img src={close} alt="Close" />
+        </button>
+      </div>
+    </div >
   );
-};
+}
 
 export default Product;
